06cdbee551dfd2c5431f15e83530f722549b8627
pi3point1415 <53444129+pi3point1415@users.noreply.github.com>
2023-07-06 19:51:16 -0600

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Code for changing subtitle
 const sub = document.getElementById("subtitle");
 
 let options;
@@ -13,3 +14,20 @@ xhttp.onreadystatechange = function() {
 
 xhttp.open("GET", "et.txt", true);
 xhttp.send();
+
+// Code for navbar
+let page = window.location.pathname.split("/").pop();
+
+console.log(page)
+
+let nav = document.getElementsByTagName("nav")[0];
+let list = nav.children[0].children
+
+for (let i = 0; i < list.length; i++) {
+    let a = list[i].children[0];
+    let navPage = a.href.split("/").pop();
+    if (navPage === page) {
+        console.log("Match found");
+        a.classList.add("active");
+    }
+}
