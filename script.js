6e189d92f7f1e88d495403ef110a358afa4b2e48
pi3point1415 <53444129+pi3point1415@users.noreply.github.com>
2023-07-07 00:48:22 -0600

    Virtual Tour

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 // Code for changing subtitle
-const sub = document.getElementById("subtitle");
+function setSubtitle() {
+    const sub = document.getElementById("subtitle");
 
-let options;
+    let options;
 
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        let result = xhttp.responseText;
-        options = result.split(/\r?\n/)
-        sub.innerHTML = options[Math.floor(Math.random() * options.length)];
-    }
-};
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let result = xhttp.responseText;
+            options = result.split(/\r?\n/)
+            sub.innerHTML = options[Math.floor(Math.random() * options.length)];
+        }
+    };
 
-xhttp.open("GET", "et.txt", true);
-xhttp.send();
-
-// Code for navbar
-let page = window.location.pathname.split("/").pop();
-
-console.log(page)
-
-let nav = document.getElementsByTagName("nav")[0];
-let list = nav.children[0].children
-
-for (let i = 0; i < list.length; i++) {
-    let a = list[i].children[0];
-    let navPage = a.href.split("/").pop();
-    if (navPage === page) {
-        console.log("Match found");
-        a.classList.add("active");
-    }
+    xhttp.open("GET", "et.txt", true);
+    xhttp.send();
 }
+
+// Code for active navbar page
+function importNavbar() {
+    let page = window.location.pathname.split("/").pop();
+
+    let nav = document.getElementsByTagName("nav")[0];
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            nav.innerHTML = xhttp.responseText;
+
+            let navList = nav.children[0].children
+
+            for (let i = 0; i < navList.length; i++) {
+                let a = navList[i].children[0];
+                let navPage = a.href.split("/").pop();
+                if (navPage === page) {
+                    console.log("Match found");
+                    a.classList.add("active");
+                }
+            }
+        }
+    }
+    xhttp.open("GET", "navbar.html", true);
+    xhttp.send();
+}
+
+setSubtitle();
+importNavbar();
