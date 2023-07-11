25d07489621c8519a1e584c9d6cda4f23492ed5f
pi3point1415 <53444129+pi3point1415@users.noreply.github.com>
2023-07-11 15:25:33 -0600

    Updated ET
    Added more ETs and added a die to randomly choose

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-// Code for changing subtitle
-function setSubtitle() {
-    const sub = document.getElementById("subtitle");
+// Currently rolling new subtitle
+let rolling = false;
+// Last subtitle
+let last = null;
+// Possible subtitles
+let options;
 
-    let options;
 
+function setSubtitleInitial() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let result = xhttp.responseText;
             options = result.split(/\r?\n/)
-            sub.innerHTML = options[Math.floor(Math.random() * options.length)];
+
+            setSubtitle();
         }
     };
 
     xhttp.open("GET", "et.txt", true);
     xhttp.send();
+}
+
+// Code for changing subtitle
+function setSubtitle() {
+    let sub = document.getElementById("subtitle");
+
+    let choice;
+    do {
+        choice = options[Math.floor(Math.random() * options.length)]
+    } while (choice === last);
+
+    last = choice;
+
+    sub.innerHTML = choice;
 }
 
 // Code for active navbar page
@@ -34,7 +52,6 @@ function importNavbar() {
                 let a = navList[i].children[0];
                 let navPage = a.href.split("/").pop();
                 if (navPage === page) {
-                    console.log("Match found");
                     a.classList.add("active");
                 }
             }
@@ -44,5 +61,34 @@ function importNavbar() {
     xhttp.send();
 }
 
-setSubtitle();
+// Code for clicking the die to randomize the ET text
+function rollDice() {
+    if (rolling) {
+        return;
+    }
+    rolling = true;
+
+    let die = document.getElementById("die");
+
+
+    let t1 = new Date().getTime();
+    let interval = 500;
+    let delay = 50;
+
+    function updateDie() {
+        let num = Math.floor(Math.random() * 6) + 1
+        die.className = "die" + num
+        if (new Date().getTime() - t1 < interval) {
+            setTimeout(updateDie, delay);
+        }
+        else {
+            setSubtitle();
+            rolling = false;
+        }
+    }
+
+    setTimeout(updateDie, delay);
+}
+
+setSubtitleInitial();
 importNavbar();
